d8262c7dc49673737518cb22ecc0d067b2c04785
Andrey Maslyuk <andrey.masliuk@3dcondox.com>
2012-11-28 23:54:56 -0500

    Server: missing files for today's commit.

---
+++ Server/vreservice/BusinessObjects/FinancialTransactionExt.cs
@@ -0,0 +1,22 @@
+﻿namespace Vre.Server.BusinessLogic
+{
+    internal static class FinancialTransactionExt
+    {
+        private const string FinancialTransactionRefNumPrefix = "3DCX";
+        private const int FinancialTransactionRefNumKey = 0x752EB3A6;
+
+        public static string GenerateRefNum(this FinancialTransaction ft)
+        {
+            return string.Format("{0}{1}",
+                FinancialTransactionRefNumPrefix,
+                Utilities.GenerateReferenceNumber(ft.AutoID ^ FinancialTransactionRefNumKey));
+        }
+
+        public static void SetAutoSystemReferenceId(this FinancialTransaction ft)
+        {
+            ft.SetSystemReferenceId(string.Format("{0}{1}",
+                FinancialTransactionRefNumPrefix,
+                Utilities.GenerateReferenceNumber(ft.AutoID ^ FinancialTransactionRefNumKey)));
+        }
+    }
+}
---
+++ Server/vreservice/Dao/FinancialTransactionDao.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using NHibernate;
+using Vre.Server.BusinessLogic;
+
+namespace Vre.Server.Dao
+{
+    internal class FinancialTransactionDao : GenericDisposableDao<FinancialTransaction, int>
+    {
+        public FinancialTransactionDao(ISession session) : base(session) { }
+
+        public FinancialTransaction[] Get(FinancialTransaction.AccountType type, int accountId)
+        {
+            return NHibernateHelper.IListToArray<FinancialTransaction>(_session.CreateQuery(
+                "FROM Vre.Server.BusinessLogic.FinancialTransaction WHERE Account=:at AND AccountId=:aid "
+                + "ORDER BY Created DESC")
+                .SetEnum("at", type)
+                .SetInt32("aid", accountId)
+                .List<FinancialTransaction>());
+        }
+
+        public FinancialTransaction[] Get(string systemRefId)
+        {
+            return NHibernateHelper.IListToArray<FinancialTransaction>(_session.CreateQuery(
+                "FROM Vre.Server.BusinessLogic.FinancialTransaction WHERE SystemRefId=:rid "
+                + "ORDER BY Created DESC")
+                .SetString("rid", systemRefId)
+                .List<FinancialTransaction>());
+        }
+    }
+}
