bda96d05b9cdcc0f466ba6000ba4e581a40c1e5d
ghsmnv <eugene.simonov@3dcondox.com>
2013-10-14 15:55:10 +0300

    Deleting dublicates. Add function remove_dublicates(KmlObject feature) in the VREstate.java

--- Clients/VREstate/src/com/condox/vrestate/client/VREstate.java
+++ Clients/VREstate/src/com/condox/vrestate/client/VREstate.java
@@ -14,25 +14,31 @@ import com.condox.clientshared.document.ViewOrder.ProductType;
 import com.condox.vrestate.client.ge.GE;
 import com.condox.vrestate.client.view.HelicopterView;
 import com.condox.vrestate.client.view.ProgressBar;
-import com.condox.vrestate.client.view.SiteView;import com.condox.vrestate.client.view._AbstractView;
+import com.condox.vrestate.client.view.SiteView;
+import com.condox.vrestate.client.view._AbstractView;
 import com.condox.vrestate.client.view.GeoItems.SiteGeoItem;
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.shared.GWT;
 import com.google.gwt.http.client.Request;
 import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.Response;
+import com.nitrous.gwt.earth.client.api.KmlContainer;
 import com.nitrous.gwt.earth.client.api.KmlObject;
+import com.nitrous.gwt.earth.client.api.KmlObjectList;
+import com.nitrous.gwt.earth.client.api.KmlPlacemark;
 import com.nitrous.gwt.earth.client.api.event.KmlLoadCallback;
 
-public class VREstate implements EntryPoint, RequestCallback, KmlLoadCallback, I_Login {
+public class VREstate implements EntryPoint, RequestCallback, KmlLoadCallback,
+		I_Login {
 
 	/**
 	 * @wbp.parser.entryPoint
 	 */
 	@Override
 	public void onModuleLoad() {
-//		Log.write(GWT.getHostPageBaseURL());
-//		Log.write(GWT.getModuleBaseForStaticFiles());
-//		Log.write(GWT.getModuleBaseURL());
+		// Log.write(GWT.getHostPageBaseURL());
+		// Log.write(GWT.getModuleBaseForStaticFiles());
+		// Log.write(GWT.getModuleBaseURL());
 		Options.Init();
 		LoginUser();
 	}
@@ -85,7 +91,8 @@ public class VREstate implements EntryPoint, RequestCallback, KmlLoadCallback, I
 				boolean useSiteModel = true;
 				for (Building bldng : Document.get().getBuildings().values()) {
 					if (bldng.getDisplayModelUrl() != "") {
-						GE.getPlugin().fetchKml(bldng.getDisplayModelUrl(),this);
+						GE.getPlugin().fetchKml(bldng.getDisplayModelUrl(),
+								this);
 						useSiteModel = false;
 					}
 					if (bldng.getOverlayUrl() != "")
@@ -95,7 +102,8 @@ public class VREstate implements EntryPoint, RequestCallback, KmlLoadCallback, I
 				}
 				if (useSiteModel)
 					if (site.getDisplayModelUrl() != "")
-						GE.getPlugin().fetchKml(site.getDisplayModelUrl(), this);
+						GE.getPlugin()
+								.fetchKml(site.getDisplayModelUrl(), this);
 			}
 
 			_AbstractView.CreateAllGeoItems();
@@ -149,8 +157,110 @@ public class VREstate implements EntryPoint, RequestCallback, KmlLoadCallback, I
 
 	@Override
 	public void onLoaded(KmlObject feature) {
-		if (feature != null)
-			GE.getPlugin().getFeatures().appendChild(feature);
+		if (feature != null) {
+			GE.getPlugin().getFeatures()
+					.appendChild(remove_dublicates(feature));
+			// // GWT.log("type: " + feature.getType());
+			// if ("KmlDocument".equals(feature.getType())) {
+			// KmlObjectList curr_placemarks = GE.getPlugin()
+			// .getElementsByType("KmlPlacemark");
+			// KmlDocument doc = (KmlDocument) feature;
+			// KmlObjectList placemarks = doc
+			// .getElementsByType("KmlPlacemark");
+			// GWT.log("curr_placemarks count: " + curr_placemarks.getLength());
+			// GWT.log("placemarks count: " + placemarks.getLength());
+			// int a = 0;
+			// while (a < placemarks.getLength()) {
+			// for (int b = 0; b < curr_placemarks.getLength(); b++) {
+			// GWT.log("a = " + a + ", b = " + b);
+			// KmlPlacemark A = (KmlPlacemark) placemarks.item(a);
+			// KmlPlacemark B = (KmlPlacemark) curr_placemarks.item(b);
+			// GWT.log("A: " + A.getKml());
+			// GWT.log("B: " + B.getKml());
+			// if (A.getName().equals(B.getName())) {
+			// GWT.log("equals!");
+			// GWT.log("doc: " + doc.getKml());
+			// // I don't know a right way here...
+			// doc = (KmlDocument) GE.parseKml(doc.getKml()
+			// .replace(A.getKml(), ""));
+			// GWT.log("doc: " + doc.getKml());
+			// break;
+			// }
+			// }
+			// a++;
+			// }
+			// GE.getPlugin().getFeatures().appendChild(doc);
+			// } else
+			// GE.getPlugin().getFeatures().appendChild(feature);
+		}
+	}
+
+	private KmlObject remove_dublicates(KmlObject feature) {
+		if ("KmlDocument".equals(feature.getType())) {
+			KmlContainer container = (KmlContainer) feature;
+
+			// To be added
+			KmlObjectList new_placemarks = container
+					.getElementsByType("KmlPlacemark");
+
+			// Already contained by GE
+			KmlObjectList curr_placemarks = GE.getPlugin().getElementsByType(
+					"KmlPlacemark");
+
+			// new placemarks
+			int i = 0;
+			int j = 0;
+			while (i < new_placemarks.getLength()) {
+				j = i + 1;
+				while (j < new_placemarks.getLength()) {
+					KmlPlacemark first = (KmlPlacemark) new_placemarks.item(i);
+					KmlPlacemark second = (KmlPlacemark) new_placemarks.item(j);
+					if (first.getName().equals(second.getName())) {
+//						container.getFeatures().removeChild(second);
+						second.setVisibility(false);
+						new_placemarks = container
+								.getElementsByType("KmlPlacemark");
+						i = 0;
+						j = 0;
+						break;
+					}
+
+					j++;
+				}
+				if (i == 0 && j == 0)
+					break;
+				i++;
+			}
+
+			// Log.write(container.getKml());
+			// new vs old placemarks
+			i = 0;
+			j = 0;
+			while (i < new_placemarks.getLength()) {
+				while (j < curr_placemarks.getLength()) {
+					KmlPlacemark first = (KmlPlacemark) new_placemarks.item(i);
+					KmlPlacemark second = (KmlPlacemark) curr_placemarks
+							.item(j);
+					// Log.write("i: " + i + ", j: " + j);
+					if (first.getName().equals(second.getName())) {
+						// I don't know why it doesn't work...
+						// container.getFeatures().removeChild(first);
+						first.setVisibility(false);
+						new_placemarks = container
+								.getElementsByType("KmlPlacemark");
+						i = 0;
+						j = 0;
+						break;
+					}
+					j++;
+				}
+				if (i == 0 && j == 0)
+					break;
+				i++;
+			}
+			return container;
+		} else
+			return feature;
 	}
 
 	@Override
