5df32a4a4c0a071b25975a1a074888d53b936bd7
zhenya <zhenya@zhenya-Lenovo-G570>
2013-12-16 20:19:45 +0200

    Save logs in list.

--- Clients/ClientShared/src/com/condox/clientshared/abstractview/Log.java
+++ Clients/ClientShared/src/com/condox/clientshared/abstractview/Log.java
@@ -1,12 +1,27 @@
 package com.condox.clientshared.abstractview;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import com.google.gwt.core.client.GWT;
 
 public class Log {
+	private static List<String> logs = new ArrayList<String>();
+	private static int maxSize = 100;
+	
 	public static void write(String message) {
-		GWT.log(message);
-//		Window.alert(message);
-//		VREstate.lbLog.addItem(message);
+		GWT.log(new Date().toString() + " >> " + message);
+		logs.add("\r\n" + new Date().toString() + " >> " + message);
+		if (logs.size() > maxSize)
+			logs.remove(logs.get(0));
 	};
+	
+	public static String getLogs() {
+		String result = "";
+		for (String log : logs)
+			result += log;
+		return result;
+	}
 
 }
