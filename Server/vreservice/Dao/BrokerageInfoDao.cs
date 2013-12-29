425c0d81645efa7d44821b82d86367462a206bce
Andrey Maslyuk <andrey.masliuk@3dcondox.com>
2013-12-29 12:34:56 -0500

    Server: Brokerage info update fix.

--- Server/vreservice/Dao/BrokerageInfoDao.cs
+++ Server/vreservice/Dao/BrokerageInfoDao.cs
@@ -8,12 +8,17 @@ namespace Vre.Server.Dao
     {
 		public BrokerageInfoDao(ISession session) : base(session) { }
 
-		public bool Exists(string name)
+		public BrokerageInfo GetByName(string name)
 		{
 			return _session.CreateQuery(
 				"FROM Vre.Server.BusinessLogic.BrokerageInfo WHERE Deleted=0 AND Name=:name")
 				.SetString("name", name)
-				.UniqueResult<BrokerageInfo>() != null;
+				.UniqueResult<BrokerageInfo>();
+		}
+
+		public bool Exists(string name)
+		{
+			return GetByName(name) != null;
 		}
 	}
 }
